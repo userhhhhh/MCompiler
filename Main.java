860426c23406466e52c327317f003db2d3c83b15
qishuo hua <145093156+userhhhhh@users.noreply.github.com>
2024-08-20 20:31:37 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
-import Util.globalScope;
+import AST.Program;
+import IR.IRPrinter;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -9,23 +10,29 @@ import java.io.InputStream;
 import parser.MxLexer;
 import parser.MxParser;
 import Util.MxErrorListener;
-import Util.error.semanticError;
-import Util.globalScope;
 import Util.error.error;
 import Frontend.ASTBuilder;
 import Frontend.SymbolCollector;
 import Frontend.SemanticChecker;
-
+import Util.Scope;
+import IR.IRProgram;
+import IR.IRBuilder;
 
 public class Main {
+    // string 相等用 equals，不能 == ！！！这样会引用传递
+    // 错误：在 build expr时要确定一部分 expr的 type，另一部分在 check expr时确定
+    // 错误：注意 expr的左值问题
+    // 错误：要添加内建函数
     public static void main(String[] args) throws Exception{
+//        String name = "C:\\Users\\31447\\IdeaProjects\\Compiler-Design-Implementation-master\\testcases\\sema\\array-package\\array-1.mx";
+//        InputStream input = System.in;
 
-        String name = "test.yx";
+        String name = "C:\\Users\\31447\\IdeaProjects\\untitled\\src\\test.mx";
         InputStream input = new FileInputStream(name);
 
         try {
-//            RootNode ASTRoot;
-            globalScope gScope = new globalScope(null);
+            Program ASTRoot;
+            Scope gScope = new Scope(null);
 
             MxLexer lexer = new MxLexer(CharStreams.fromStream(input));
             lexer.removeErrorListeners();
@@ -33,12 +40,26 @@ public class Main {
             MxParser parser = new MxParser(new CommonTokenStream(lexer));
             parser.removeErrorListeners();
             parser.addErrorListener(new MxErrorListener());
-            ParseTree parseTreeRoot = parser.program();
+            ParseTree parseTreeRoot = null;
+            try{
+                parseTreeRoot = parser.program();
+            } catch (Exception e) {
+                System.out.println("Invalid Identifier");
+                System.exit(1);
+            }
 
-//            ASTBuilder astBuilder = new ASTBuilder(gScope);
-//            ASTRoot = (RootNode)astBuilder.visit(parseTreeRoot);
-//            new SymbolCollector(gScope).visit(ASTRoot);
-//            new SemanticChecker(gScope).visit(ASTRoot);
+
+            ASTBuilder astBuilder = new ASTBuilder(gScope);
+            ASTRoot = (Program)astBuilder.visit(parseTreeRoot);
+            new SymbolCollector(gScope).visit(ASTRoot);
+            gScope.visitInfunc();
+            gScope.visitInclass();
+            new SemanticChecker(gScope).visit(ASTRoot);
+
+            IRProgram irProgram = new IRProgram();
+            new IRBuilder(irProgram, gScope).visit(ASTRoot);
+            IRPrinter irPrinter = new IRPrinter(irProgram);
+            irPrinter.print();
 
 //            mainFn f = new mainFn();
 //            new IRBuilder(f, gScope).visit(ASTRoot);
@@ -49,6 +70,7 @@ public class Main {
 //            new RegAlloc(asmF).work();
 //            new AsmPrinter(asmF, System.out).print();
         } catch (error er) {
+//            System.exit(1);
             System.err.println(er.toString());
             throw new RuntimeException();
         }
